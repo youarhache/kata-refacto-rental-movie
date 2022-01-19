ddd7904d05e460a8046c38ce734ebca8d07d7a81
youarhache <youarhache@gmail.com>
2022-01-19 11:29:27 +0100

    moving the price calculation and frequent renter points to Rental class

--- python/movie_rental/customer.py
+++ python/movie_rental/customer.py
@@ -1,7 +1,7 @@
 from nis import match
 from tokenize import Double
 from typing import List
-from python.movie_rental.movie import Movie, PriceCodes
+from python.movie_rental.movie import PriceCodes
 from python.movie_rental.rental import Rental
 
 
@@ -22,33 +22,20 @@ class Customer:
         frequent_renter_points = 0
         result = "Rental Record for " + self.name + "\n"
 
-        for each in self._rentals:
-            this_amount = 0.0
-
-            #//determine amounts for each line
-            match each.movie.price_code:
-                case PriceCodes.REGULAR:
-                    this_amount += 2
-                    if each.days_rented > 2:
-                        this_amount += (each.days_rented - 2) * 1.5
-                case PriceCodes.NEW_RELEASE:
-                    this_amount += each.days_rented * 3
-                case PriceCodes.CHILDRENS:
-                    this_amount += 1.5
-                    if each.days_rented > 3:
-                        this_amount += (each.days_rented - 3) * 1.5
+        for rental in self._rentals:
+            this_amount = rental.get_amount()
+            total_amount += this_amount
 
             # add frequent renter points
-            frequent_renter_points += 1
-            if (each.movie.price_code == PriceCodes.NEW_RELEASE) and each.days_rented > 1:
-                frequent_renter_points += 1
+            frequent_renter_points += rental.get_frequent_renter_points()
 
             # show figures for this rental
-            result += "\t" + each.movie.title + "\t" + str(this_amount) + "\n"
-            total_amount += this_amount
+            result += "\t" + rental.movie.title + "\t" + str(this_amount) + "\n"
 
         # add footer lines
         result += "Amount owed is " + str(total_amount) + "\n"
-        result += "You earned " + str(frequent_renter_points) + " frequent renter points"
+        result += (
+            "You earned " + str(frequent_renter_points) + " frequent renter points"
+        )
 
         return result
--- python/movie_rental/rental.py
+++ python/movie_rental/rental.py
@@ -1,7 +1,30 @@
-from python.movie_rental.movie import Movie
+from python.movie_rental.movie import Movie, PriceCodes
 
 
 class Rental:
     def __init__(self, movie: Movie, days_rented: int) -> None:
         self.movie = movie
         self.days_rented = days_rented
+
+    def get_amount(self):
+        amount = 0.0
+        match self.movie.price_code:
+            case PriceCodes.REGULAR:
+                amount += 2
+                if self.days_rented > 2:
+                    amount += (self.days_rented - 2) * 1.5
+            case PriceCodes.NEW_RELEASE:
+                amount += self.days_rented * 3
+            case PriceCodes.CHILDRENS:
+                amount += 1.5
+                if self.days_rented > 3:
+                    amount += (self.days_rented - 3) * 1.5
+        return amount
+
+    def get_frequent_renter_points(self):
+        frequent_renter_points = 1
+        if (
+            self.movie.price_code == PriceCodes.NEW_RELEASE
+        ) and self.days_rented > 1:
+            frequent_renter_points += 1
+        return frequent_renter_points
