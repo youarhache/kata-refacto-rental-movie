7201046456d3b4567b370582b683f5f6729fe87d
youarhache <youarhache@gmail.com>
2022-01-19 14:08:08 +0100

    replacing magic numbers with constants

--- python/movie_rental/rental.py
+++ python/movie_rental/rental.py
@@ -22,47 +22,58 @@ class Rental(ABC):
 
 
 class RegularRental(Rental):
+    BASE_PRICE = 2.0
+    DAILY_PRICE = 1.5
+    ADDITIONAL_FEE_STARING_DAY = 2
+
     def __init__(self, movie: Movie, days_rented: int) -> None:
         super().__init__(movie, days_rented)
 
     def get_price(self) -> float:
-        return 2.0 + self._get_long_rental_additional_fees()
+        return self.BASE_PRICE + self._get_long_rental_additional_fees()
 
     def _get_long_rental_additional_fees(self) -> float:
-        if self.days_rented <= 2:
+        if self.days_rented <= self.ADDITIONAL_FEE_STARING_DAY:
             return 0
-        return (self.days_rented - 2) * 1.5
+        return (self.days_rented - self.ADDITIONAL_FEE_STARING_DAY) * self.DAILY_PRICE
 
     def get_frequent_renter_points(self) -> int:
         return 1
 
 
 class NewReleaseRental(Rental):
+    DAILY_PRICE = 3.0
+    BONUS_POINTS_STARING_DAY = 1
+
     def __init__(self, movie: Movie, days_rented: int) -> None:
         super().__init__(movie, days_rented)
 
     def get_price(self) -> float:
-        return self.days_rented * 3.0
+        return self.days_rented * self.DAILY_PRICE
 
     def get_frequent_renter_points(self) -> int:
         frequent_renter_points = 1 + self._get_bonus_points()
         return frequent_renter_points
 
     def _get_bonus_points(self) -> int:
-        return int(self.days_rented > 1)
+        return int(self.days_rented > self.BONUS_POINTS_STARING_DAY)
 
 
 class ChildrenMovieRental(Rental):
+    BASE_PRICE = 1.5
+    DAILY_PRICE = 1.5
+    ADDITIONAL_FEE_STARING_DAY = 3
+
     def __init__(self, movie: Movie, days_rented: int) -> None:
         super().__init__(movie, days_rented)
 
     def get_price(self) -> float:
-        return 1.5 + self._get_long_rental_additional_fees()
+        return self.BASE_PRICE + self._get_long_rental_additional_fees()
 
     def _get_long_rental_additional_fees(self) -> float:
-        if self.days_rented <= 3:
+        if self.days_rented <= self.ADDITIONAL_FEE_STARING_DAY:
             return 0
-        return (self.days_rented - 3) * 1.5
+        return (self.days_rented - self.ADDITIONAL_FEE_STARING_DAY) * self.DAILY_PRICE
 
     def get_frequent_renter_points(self) -> int:
         return 1
